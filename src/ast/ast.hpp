6be513e70dede63e1510554f89126b9d59434061
Tom Brossard <tombrossard0@gmail.com>
2025-08-23 15:46:09 +0200

    feat: more automation for ast

--- src/ast/ast.hpp
+++ src/ast/ast.hpp
@@ -13,11 +13,14 @@
 #define FIELD_INIT(type, name) name(std::move(name)),
 #define FIELD_INIT_END(type, name) name(std::move(name))
 
-#define DEFINE_AST(name, FIELDS)                                                                             \
+#define DEFINE_AST_SUBCLASS(name, FIELDS)                                                                    \
     class name : public Expr {                                                                               \
       public:                                                                                                \
         FIELDS(FIELD_MEMBER, FIELD_MEMBER)                                                                   \
         name(FIELDS(FIELD_PARAMS, FIELD_PARAMS_END)) : FIELDS(FIELD_INIT, FIELD_INIT_END) {                  \
+        }                                                                                                    \
+        template <typename T> T accept(class Visitor<T> &visitor) {                                          \
+            return visitor.visit##name##Expr(*this);                                                         \
         }                                                                                                    \
     };
 
@@ -32,14 +35,24 @@ class Expr {
 using UniqueExpr = std::unique_ptr<Expr>;
 
 // === Generated definitions ===
+
 #define BINARY_FIELDS(X, Y) X(UniqueExpr, left) X(Token, op) Y(UniqueExpr, right) // Y = no comma at the end
-DEFINE_AST(Binary, BINARY_FIELDS)
-
 #define GROUPING_FIELDS(X, Y) Y(UniqueExpr, expression)
-DEFINE_AST(Grouping, GROUPING_FIELDS)
-
 #define LITERAL_FIELDS(X, Y) Y(Object, value)
-DEFINE_AST(Literal, LITERAL_FIELDS)
-
 #define UNARY_FIELDS(X, Y) X(UniqueExpr, op) Y(UniqueExpr, right)
-DEFINE_AST(Unary, UNARY_FIELDS)
+
+#define AST_NODES(X)                                                                                         \
+    X(Binary, BINARY_FIELDS)                                                                                 \
+    X(Grouping, GROUPING_FIELDS)                                                                             \
+    X(Literal, LITERAL_FIELDS)                                                                               \
+    X(Unary, UNARY_FIELDS)
+
+template <typename T> class Visitor; // forward declaration
+
+AST_NODES(DEFINE_AST_SUBCLASS)
+
+template <typename T> class Visitor {
+#define VISIT_METHOD(name, FIELDS) virtual T visit##name##Expr(name &expr) = 0;
+    AST_NODES(VISIT_METHOD)
+#undef VISIT_METHOD
+};
