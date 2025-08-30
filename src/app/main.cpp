94662313593dc178397fbb7d7e1d25889000b0ea
Tom Brossard <tombrossard0@gmail.com>
2025-08-30 11:27:33 +0200

    chore: delete interpreter for now

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -21,9 +21,6 @@ int compile(std::vector<UniqueStmt> &statements, std::string filename) {
     for (auto &statement : statements) {
         AstPrinter printer{};
         printer.print(*statement);
-
-        // Interpreter interpreter{};
-        // interpreter.interpret(*statement);
     }
 
     // Generate IR
@@ -81,15 +78,6 @@ int compile(std::vector<UniqueStmt> &statements, std::string filename) {
 // We follow the conventions defined in UNIX "sysexits.h" header for exit codes:
 // (https://man.freebsd.org/cgi/man.cgi?query=sysexits&apropos=0&sektion=0&manpath=FreeBSD+4.3-RELEASE&format=html).
 int main(int argc, char **argv) {
-    // if (argc > 2) {
-    //     std::cerr << "Usage: marbl [script]" << std::endl;
-    //     return EX_USAGE;
-    // }
-
-    // if (argc == 2) { return Marbl::runFile(argv[1]); }
-
-    // return Marbl::runPrompt();
-
     std::ifstream inputFile(argv[1]);
 
     if (!inputFile) {
--- src/marbl/marbl.cpp
+++ src/marbl/marbl.cpp
@@ -3,7 +3,6 @@
 #include <sstream>
 #include <vector>
 
-#include "interpreter.hpp"
 #include "lexer.hpp"
 #include "parser.hpp"
 #include "printer.hpp"
@@ -23,9 +22,6 @@ int Marbl::runFile(char *filepath) {
     for (auto &statement : statements) {
         AstPrinter printer{};
         printer.print(*statement);
-
-        // Interpreter interpreter{};
-        // interpreter.interpret(*statement);
     }
 
     inputFile.close();
@@ -37,34 +33,6 @@ int Marbl::runFile(char *filepath) {
 
 int Marbl::runPrompt() {
     std::string line;
-
-    // while (true) {
-    //     std::cout << "> ";
-    //     if (!std::getline(std::cin, line)) break; // EOF
-
-    //     if (line.empty()) continue;
-
-    //     std::stringstream ss(line);
-    //     Parser parser{ss};
-    //     if (parser.lexer.currentToken.type == T_EOF) parser.lexer.nextToken(); // RESET
-
-    //     while (!parser.isAtEnd()) {
-    //         UniqueExpr expression;
-    //         expression = parser.parse();
-    //         if (!expression) {
-    //             std::cerr << "Parse returned null expression." << std::endl;
-    //             hadError = true;
-    //             continue;
-    //         }
-
-    //         AstPrinter printer{};
-    //         printer.print(*expression);
-
-    //         Interpreter interpreter{};
-    //         interpreter.interpret(*expression);
-    //     }
-    // }
-
     std::cout << std::endl;
 
     return hadError ? EX_DATAERR : EX_OK;
